24501978cf563e42c88097a8546ad5e2abe5d999
mmacieli <mmacielinski@splunk.com>
2020-05-11 16:08:17 -0400

    Create create_hec_event_sample.ps1

---
+++ Powershell/create_hec_event_sample.ps1
@@ -0,0 +1,26 @@
+write-host "$computer_name CPU LOAD:$cpu_load"
+
+$response = ""
+ $formatteddate = "{0:MM/dd/yyyy hh:mm:sstt zzz}" -f (Get-Date)
+ $arraySeverity = 'INFO','WARN','ERROR'
+ $severity = $arraySeverity[(Get-Random -Maximum ([array]$arraySeverity).count)]
+ 
+ $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+ #need to setup HEC on an Indexer or HF and provide token
+ $headers.Add("Authorization", 'Splunk xxxxx-xxx-xxxx-xxxx-xxxxxxxxxxxx')
+ 
+ $body = '{
+         "host":"' + $env:computername + '",
+         "sourcetype":"testevents",
+         "source":"Geoff''s PowerShell Script",
+         "event":{
+             "message":"Something Happened on host ' + $env:computername + '",
+             "severity":"' + $severity + '",
+             "user": "'+ $env:username + '",
+             "date":"' + $formatteddate + '"
+             }
+         }'
+ #provide ip or DNS name of HEC configured host
+ $splunkserver = "http://xxx.xxx.xxx.xxx:8088/services/collector"
+ $response = Invoke-RestMethod -Uri $splunkserver -Method Post -Headers $headers -Body $body
+ "Code:'" + $response.code + "' text:'"+ $response.text + "'" 
